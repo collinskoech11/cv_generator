cbc2f32e4bb36377a5e9be78aa60266136db1ba5
collinskoech11 <collinskoechck34@gmail.com>
2021-12-05 10:57:49 +0300

    Added Details View

--- cv/views.py
+++ cv/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def Details(request):
+    return render(request, 'Details.html')
