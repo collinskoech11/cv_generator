583ec58bb23bbec14a76e16bcba72938ff9a696f
collinskoech11 <collinskoechck34@gmail.com>
2021-12-07 00:28:27 +0300

    Personal DEtails Model

--- cv/models.py
+++ cv/models.py
@@ -4,9 +4,9 @@ from django.db import models
 class Personal(models.Model):
     def __str__(self):
         return self.FullName
-FullName = models.CharField(max_length=200)
-location = models.CharField(max_length=200)
-email = models.CharField(max_length=200)
-social = models.CharField(max_length=200)
-PhoneNumber = models.CharField(max_length=200)
-PersonalSummary = models.CharField(max_length=400)
+    FullName = models.CharField(max_length=200)
+    location = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    social = models.CharField(max_length=200)
+    PhoneNumber = models.CharField(max_length=200)
+    PersonalSummary = models.CharField(max_length=400)
