d0d9c48200ce5a79642181866a887860ce0293bf
collinskoech11 <collinskoechck34@gmail.com>
2021-12-07 12:12:06 +0300

    update models.py

--- cv/models.py
+++ cv/models.py
@@ -10,3 +10,24 @@ class Personal(models.Model):
     social = models.CharField(max_length=200)
     PhoneNumber = models.CharField(max_length=200)
     PersonalSummary = models.CharField(max_length=400)
+
+class Experiece(models.Model):
+    def __str__(self):
+        return self.Role
+    Role = models.CharField(max_length = 200)
+    CompanyName = models.CharField(max_length=200)
+    Timeline = models.CharField(max_length=200)
+
+class Education(models.Model):
+    SchoolName = models.CharField(max_length=200)
+    Course = models.CharField(max_length=200)
+    Timeline = models.CharField(max_length=200)
+
+class Skill(models.Model):
+    Skills = models.CharField(max_length=200)
+    
+
+class Award(models.Model):
+    Awards = models.CharField(max_length=200)
+    
+
